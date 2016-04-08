dc70f43600f3a5d2889d855e0816a22eb210f374
Attila Blenesi <battila@halcyonmobile.com>
2016-04-08 08:53:12 +0300

    [Lab 4] Dependency injection with Singleton Factory with setter method removed file manager form LogAnalyser

--- src/main/java/ro/ubbcluj/scs/LogAnalyser.java
+++ src/main/java/ro/ubbcluj/scs/LogAnalyser.java
@@ -5,18 +5,14 @@ package ro.ubbcluj.scs;
  */
 public class LogAnalyser {
 
-    private FileManager fileManager;
-
-    public LogAnalyser() {
-        this.fileManager = FileManagerFactory.getInstance().getFileManager();
-    }
+    public LogAnalyser() {}
 
     public boolean isValidLogFileName(String name) {
         if (name != null) {
-            if (name.length() < fileManager.getLogNameExt().length() + 2) {
+            if (name.length() < FileManagerFactory.getInstance().getFileManager().getLogNameExt().length() + 2) {
                 throw new IllegalArgumentException("Name is to short.");
             }
-            return name.endsWith("." + fileManager.getLogNameExt());
+            return name.endsWith("." + FileManagerFactory.getInstance().getFileManager().getLogNameExt());
         }
         return false;
     }
